edbacb6fe3f679d4063cd661d272aaf2c2cb4688
amogharajsandur <s865music@gmail.com>
2025-07-30 15:12:05 +0530

    Completed : ReactFacts Project - Markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+Challenge: Starting from scratch, build and render the
+HTML for our section project. Check the Google slide for
+what you're trying to build.
+We'll be adding more styling to it later.
+Hints:
+* The React logo is a file in the project tree, so you can
+access it by using 'src="react-logo.png"' in your image
+element
+* You can also set the 'width' attribute of the image element
+just like in HTML. In the slide, I have it set to 40px
+
+Content:
+Fun facts about React
+• Was first released in 2013
+• Was originally created by Jordan Walke
+• Has well over 100K stars on GitHub
+• Is maintained by Meta
+• Powers thousands of enterprise apps, including
+mobile apps
+*/
+
+import "./index.css"
+import ReactLogo from "./assets/react.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={ReactLogo} alt="React Logo" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </>
   )
 }
