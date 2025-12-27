a7415d6b85873d0fcfb3e306eae2f10c374754c0
amogharajsandur <amogharaj.sandur@yahoo.com>
2025-12-27 12:09:58 +0530

    refactor: Modernize React rendering with `createRoot` and `StrictMode`, centralize CSS import, and correct a typo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css"
-import React, { Fragment } from "react";
-import Header from "./components/header.jsx"
+import { Fragment } from "react";
+import Header from "./components/Header.jsx"
 import MainContent from "./components/MainContent.jsx"
 
 export default function App() {
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -6,7 +6,7 @@ export default function MainContent() {
             <h1>Fun facts about React</h1>
             <ul>
                 <li>Was first released in 2013</li>
-                <li>Was originally created bh Walke</li>
+                <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Meta</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
